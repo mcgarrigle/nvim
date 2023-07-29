0fcfc91b66f83cde381b652975def00e3545102f
Peter McGarrigle <mcgarrigle@github>
2023-07-29 21:34:43 +0000

    added some basic defaults

--- lua/basics.lua
+++ lua/basics.lua
@@ -2,4 +2,11 @@ vim.wo.number = true
 vim.opt.guicursor = ""
 vim.g.mapleader = " "
 
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2 -- 1 tab = 2 spaces
+vim.opt.shiftwidth = 2 -- indentation rule
+vim.opt.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
+vim.opt.expandtab = true -- expand tab to spaces
+
 require'nvim-lastplace'.setup{}
