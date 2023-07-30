f92947586716544906886ab61e15277c273e67e9
Peter McGarrigle <mcgarrigle@github>
2023-07-30 21:12:49 +0000

    added oil.vim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -47,5 +47,10 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim",
      tag = "0.1.2",
      dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" }
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   }
 })
