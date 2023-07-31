d1d5d1c45b0514441cb88b431238d2ce499f6a57
Peter McGarrigle <mcgarrigle@github>
2023-07-31 08:00:48 +0000

    keymaps for jq tool

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set('n', '<leader><Down>',  '<cmd>enew<cr>i', {})
 
 -- clear highlights
 vim.keymap.set('n', '<leader>n', '<cmd>noh<cr>', {})
+
+vim.keymap.set('n', '<leader>jj', '<cmd>%!jq -S .<cr>', {})
+vim.keymap.set('n', '<leader>jc', '<cmd>%!jq -c .<cr>', {})
