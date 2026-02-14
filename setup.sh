301f32532dcd4a11caa257110689fa0ad2ee5a09
pete <mcgarrigle@github.com>
2026-02-14 22:09:36 +0000

    setup.sh removes old version

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ ARCH=$(uname -m)
 [ $ARCH == aarch64 ] && ARCH=arm64
 
 DIR="${HOME}/.local/share"
+PACKAGE="nvim-linux-${ARCH}"
 
 mkdir -p "${DIR}" && cd "${DIR}"
 
-curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-${ARCH}.tar.gz|tar xz
-ln -sf "${DIR}/nvim-linux-${ARCH}" "${DIR}/nvim"
+rm -rf "${DIR}/${PACKAGE}"
+curl -L https://github.com/neovim/neovim/releases/latest/download/${PACKAGE}.tar.gz|tar xz
+ln -sf "${DIR}/${PACKAGE}" "${DIR}/nvim"
 
 ls -l $DIR
